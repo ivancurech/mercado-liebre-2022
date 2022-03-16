a0ca7415c839cf0874b9e632023c44d9ea77bc70
Ivan-Cure-Chegwin <ivancurech@gmail.com>
2022-03-15 20:26:44 -0500

    agregando variable de entorno port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,"../", "public")));
 
@@ -9,4 +9,4 @@ app.use(express.static(path.join(__dirname,"../", "public")));
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 
 
-app.listen(port, () => console.log("estoy funcionando en el puerto " + port));
+app.listen(PORT, () => console.log("estoy funcionando en el puerto " + PORT));
