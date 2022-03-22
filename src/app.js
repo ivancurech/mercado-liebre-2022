383046187c6cae5ceaa79d13e463b12cb00aa4e4
Ivan-Cure-Chegwin <ivancurech@gmail.com>
2022-03-21 20:45:00 -0500

    agregando formularios de registro y login

--- src/app.js
+++ src/app.js
@@ -4,9 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,"../", "public")));
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "home.html")))
 
+app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "login.html")))
+
+app.get("/registro", (req, res) => res.sendFile(path.join(__dirname, "views", "registro.html")))
+
+app.post("/registro", (req, res) => res.send("usuario registrado"))
 
 app.listen(PORT, () => console.log("estoy funcionando en el puerto " + PORT));
